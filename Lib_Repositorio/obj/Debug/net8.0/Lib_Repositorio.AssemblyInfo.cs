f113fe554badea188dbe524c49ddb3492641f234
isabella066 <isabellacordobatamayo06@gmail.com>
2025-05-16 20:47:52 -0500

    agregamos libreria de prueba

--- Lib_Repositorio/obj/Debug/net8.0/Lib_Repositorio.AssemblyInfo.cs
+++ Lib_Repositorio/obj/Debug/net8.0/Lib_Repositorio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lib_Repositorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84bd92b887234877c03478f24cdfa0159ddb1f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lib_Repositorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lib_Repositorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ lib_prueba/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace lib_prueba
+{
+    public class Class1
+    {
+
+    }
+}
